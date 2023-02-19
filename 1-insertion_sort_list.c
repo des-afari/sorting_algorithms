35376def8d2d040f3be677be06ae5c35385503c0
des-afari <afaridesmond@gmail.com>
2023-02-19 17:55:34 +0000

    sorted list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,39 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- *                        order using the Insertion sort algorithm
- *
- * @list: Address of pointer to the head of the list
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ * @list: double pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr, *prev, *next;
+	listint_t *curr, *swap;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    curr = (*list)->next;
-
-    while (curr != NULL)
-    {
-        prev = curr->prev;
-        next = curr->next;
-
-        while (prev != NULL && prev->n > curr->n)
-        {
-            /* Swap curr and prev nodes */
-            if (prev->prev != NULL)
-                prev->prev->next = curr;
-            else
-                *list = curr;
-
-            if (curr->next != NULL)
-                curr->next->prev = prev;
-
-            prev->next = next;
-            curr->prev = prev->prev;
-            prev->prev = curr;
-            curr->next = prev;
-
-            print_list(*list);
-
-            prev = curr->prev;
-        }
-
-        curr = next;
-    }
+	for (curr = (*list)->next; curr != NULL; curr = curr->next)
+	{
+		for (swap = curr->prev; swap != NULL; swap = swap->prev)
+		{
+			if (swap->n > curr->n)
+			{
+				if (swap->prev != NULL)
+					swap->prev->next = curr;
+				else
+					*list = curr;
+				curr->prev = swap->prev;
+				swap->prev = curr;
+				swap->next = curr->next;
+				if (curr->next != NULL)
+					curr->next->prev = swap;
+				curr->next = swap;
+				print_list(*list);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
 }
